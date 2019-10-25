6570386aca3a8898c1ed9445037b8302f375264a
Bailee Beckles <bbeckles@spartaglobal.com>
2019-10-25 09:44:50 +0100

    first commit

---
+++ src/main/java/com/spartaglobal/IO_Exceptions/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.IO_Exceptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
